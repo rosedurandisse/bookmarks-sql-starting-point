c651f46fe7ce9d56bd64e64e16003f533bada0da
Rose Durandisse <rosedurandisse@pursuit.org>
2022-02-01 20:30:02 -0500

    Added queries to access all transactions, 1 transaction, and post a transaction

--- queries/bookmarks.js
+++ queries/bookmarks.js
@@ -2,12 +2,33 @@ const db = require("../db/dbConfig");
 
 const getAllBookmarks = async () => {
   try {
-    const bookmarks = await db.any("SELECT * from bookmarks");
-
+    const bookmarks = await db.any("SELECT * FROM bookmarks");
     return bookmarks;
   } catch (error) {
     return error;
   }
 };
 
-module.exports = getAllBookmarks;
+const getBookmark = async (id) => {
+  try {
+    const bookmark = await db.one("SELECT * FROM bookmarks WHERE id=$1", id);
+    return bookmark;
+  } catch (error) {
+    return error;
+  }
+};
+
+const createBookmark = async (bookmark) => {
+  const { name, url, category, is_favorite } = bookmark;
+  const createdBookmark = await db.one(
+    "INSERT INTO bookmarks (name, url, category, is_favorite) VALUES ($1, $2, $3, $4) RETURNING *",
+    [name, url, category, is_favorite]
+  );
+  return createdBookmark;
+};
+
+module.exports = {
+  getAllBookmarks,
+  getBookmark,
+  createBookmark,
+};
