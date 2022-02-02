1c3886e69a305e3e617aff9a71cbdfb8dea6ae1f
Rose Durandisse <rosedurandisse@pursuit.org>
2022-02-01 20:29:02 -0500

    Added get all, get id, and post

--- controllers/bookmarksController.js
+++ controllers/bookmarksController.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const { json } = require("express/lib/response");
 
-const getAllBookmarks = require("../queries/bookmarks");
+const {
+  getAllBookmarks,
+  getBookmark,
+  createBookmark,
+} = require("../queries/bookmarks");
 
 const bookmarks = express.Router();
 
@@ -8,12 +13,23 @@ bookmarks.get("/", async (_, response) => {
   console.log("GET request to /bookmarks");
   const allBookmarks = await getAllBookmarks();
   if (allBookmarks.length === 0) {
-    res.status(500).json({ error: "server error" });
-
+    response.status(500).json({ error: "server error" });
     return;
   }
-
   response.status(200).json(allBookmarks);
+});
+
+bookmarks.get("/:id", async (request, response) => {
+  console.log("I got one bookmark");
+  const oneBookmark = await getBookmark(request.params.id);
+  response.status(200).json(oneBookmark);
+});
+
+bookmarks.post("/", async (request, response) => {
+  console.log(request.body);
+  // console.log("I added a bookmark");
+  // const addedBookmark = await createBookmark(request.body);
+  // response.status(200).json(addedBookmark);
 });
 
 module.exports = bookmarks;
